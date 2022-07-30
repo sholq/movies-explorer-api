f37aa780802f974a370c9fe3518afbdeba997be6
sholq <semyon.silkin@gmail.com>
2022-07-30 13:26:26 +0300

    feat: add app.js and logger middleware

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+// const { errors, celebrate, Joi } = require('celebrate');
+
+require('dotenv').config();
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+mongoose.connection.on('connected', () => console.log('Connected'));
+mongoose.connection.on('error', (err) => console.log('Connection failed with - ', err));
+
+// const { usersRouter } = require('./routes/users');
+// const { cardsRouter } = require('./routes/cards');
+// const { notFoundRouter } = require('./routes/not_found');
+
+// const { createUser, login } = require('./controllers/auth');
+
+// const auth = require('./middlewares/auth');
+// const { handleErrors } = require('./middlewares/errors');
+// const cors = require('./middlewares/cors');
+
+const { urlRegEx } = require('./regex/regex');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log({ headers: req.headers, body: req.body });
+  console.log('Запрос залогирован!');
+  next();
+});
+app.use(requestLogger);
+app.use(limiter);
+// app.use(cors);
+// app.post('/signin', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required().min(8),
+//   }),
+// }), login);
+// app.post('/signup', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required().min(8),
+//     name: Joi.string().min(2).max(30),
+//     about: Joi.string().min(2).max(30),
+//     avatar: Joi.string().regex(urlRegEx),
+//   }),
+// }), createUser);
+// app.use(auth);
+// app.use('/users', usersRouter);
+// app.use('/cards', cardsRouter);
+// app.use('/', notFoundRouter);
+app.use(errorLogger);
+// app.use(errors());
+// app.use(handleErrors);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
