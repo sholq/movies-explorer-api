0da9b41d57f7825f32b677f46a634acb5b78d174
sholq <semyon.silkin@gmail.com>
2022-08-03 18:30:25 +0300

    fix: fix not found route error

--- routes/not_found.js
+++ routes/not_found.js
@@ -2,6 +2,6 @@ const notFoundRouter = require('express').Router();
 
 const NotFoundError = require('../errors/not-found-error');
 
-notFoundRouter.all('/*', (req, res, next) => next(new NotFoundError('Некорректные данные')));
+notFoundRouter.all('/*', (req, res, next) => next(new NotFoundError('Страница не найдена')));
 
 module.exports = { notFoundRouter };
